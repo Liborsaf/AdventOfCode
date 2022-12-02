43e1454343f12dc0d60d332497d7eb50f50b509d
Libor Šafář <liborsaf@wetian.eu>
2022-12-02 08:40:31 +0100

    Small refactor
    Took 5 minutes

--- src/tasks.py
+++ src/tasks.py
@@ -1,11 +1,21 @@
 from enum import Enum
-from typing import Optional
+from typing import Optional, List
 
 from aoc import AdventOfCodeTask
 
 
 class FirstDayTask(AdventOfCodeTask):
     def run(self):
+        elves = self.sum_elves()
+
+        sorted_elves = sorted(elves, reverse=True)
+
+        most_elf = sorted_elves[0]
+        most_three_elves = sum(sorted_elves[:3])
+
+        print(f"Most elf total: {most_elf}, Most 3 elves total: {most_three_elves}")
+
+    def sum_elves(self) -> List[int]:
         elves = []
         elf_calories = 0
 
@@ -18,12 +28,7 @@ class FirstDayTask(AdventOfCodeTask):
 
             elf_calories += int(data)
 
-        sorted_elves = sorted(elves, reverse=True);
-
-        most_elf = sorted_elves[0]
-        most_three_elves = sum(sorted_elves[:3])
-
-        print(f"Most elf total: {most_elf}, Most 3 elves total: {most_three_elves}")
+        return elves
 
 
 class SecondDayTask(AdventOfCodeTask):
