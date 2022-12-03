c48eb9d0ed077f80353f4e1cede831f4e7cf3f25
Libor Šafář <liborsaf@wetian.eu>
2022-12-03 02:45:08 +0100

    Just another save commit after refactor
    Took 14 minutes

--- src/tasks/year_2015/second_day.py
+++ src/tasks/year_2015/second_day.py
@@ -2,11 +2,49 @@
 from aoc import AdventOfCodeTask
 
 
+class PresentFace:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def area(self) -> int:
+        return self.width * self.height
+
+
+class Present:
+    def __init__(self, length: int, width: int, height: int):
+        self.first_face = PresentFace(length, width)
+        self.second_face = PresentFace(width, height)
+        self.third_face = PresentFace(height, length)
+
+        self.bow_ribbon_length = length * width * height
+
+    def get_smallest_face(self):
+        # Precalculate areas of all sides
+        first_face_area = self.first_face.area()
+        second_face_area = self.second_face.area()
+        third_face_area = self.third_face.area()
+
+        # Get area of the smallest side
+        smallest_face_area = min(first_face_area, second_face_area, third_face_area)
+
+        if smallest_face_area == first_face_area:
+            return self.first_face
+        elif smallest_face_area == second_face_area:
+            return self.second_face
+        elif smallest_face_area == third_face_area:
+            return self.third_face
+
+    def surface_area(self) -> int:
+        # Calculate box surface area
+        return 2 * self.first_face.area() + 2 * self.second_face.area() + 2 * self.third_face.area()
+
+
 # https://adventofcode.com/2015/day/2
 class SecondDayTask(AdventOfCodeTask):
     def run(self):
-        needed_wrapping_paper = 0
-        needed_ribbon = 0
+        total_needed_wrapping_paper = 0
+        total_needed_ribbon = 0
 
         for data in self.task_input.split("\n"):
             # Skip last empty line
@@ -14,23 +52,15 @@ class SecondDayTask(AdventOfCodeTask):
                 continue
 
             (length, width, height) = [int(value) for value in data.split("x")]
-            bow_ribbon = length * width * height
 
-            # Calculate all 3 sides areas
-            first_face_area = length * width
-            second_face_area = width * height
-            third_face_area = height * length
-            # Get area of the smallest side
-            smallest_face_area = min(first_face_area, second_face_area, third_face_area)
+            present = Present(length, width, height)
+            present_smallest_face = present.get_smallest_face()
 
-            # Calculate box surface area
-            box_surface_area = 2 * first_face_area + 2 * second_face_area + 2 * third_face_area
-            # Sum total needed area, some reserve
-            box_total_needed_area = box_surface_area + smallest_face_area
+            # Sum total needed wrapping paper with some reserve
+            present_needed_wrapping_paper = present.surface_area() + present_smallest_face.area()
+            present_needed_ribbon = present.bow_ribbon_length
 
-            total_needed_ribbon = bow_ribbon
+            total_needed_wrapping_paper += present_needed_wrapping_paper
+            total_needed_ribbon += total_needed_ribbon
 
-            needed_wrapping_paper += box_total_needed_area
-            needed_ribbon += total_needed_ribbon
-
-        print(f"Needed wrapping paper: {needed_wrapping_paper}, needed ribbon: {needed_ribbon}")
+        print(f"Needed wrapping paper: {total_needed_wrapping_paper}, needed ribbon: {total_needed_ribbon}")
