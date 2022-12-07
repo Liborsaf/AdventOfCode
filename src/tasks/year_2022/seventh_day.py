a0b4a732dfa7eb63a4c2e6d56507dbd192a36f58
Libor Šafář <liborsaf@wetian.eu>
2022-12-07 06:43:55 +0100

    Save commit
    Took 19 minutes

--- src/tasks/year_2022/seventh_day.py
+++ src/tasks/year_2022/seventh_day.py
@@ -1,15 +1,53 @@
+import copy
+from typing import List, Deque
+from collections import deque
+
 # noinspection PyPackageRequirements
 from aoc import AdventOfCodeTask
+
+
+class File:
+    def __init__(self, name: str, size: int):
+        self.name = name
+        self.size = size
+
+
+class Directory(File):
+    def __init__(self, name: str):
+        super().__init__(name, 0)
+
+        self.content = {}
+
+
+class FileSystem(Directory):
+    def __init__(self):
+        super().__init__('/')
+
+    def get_folder(self, path: Deque[str] | str) -> Directory:
+        pass
+
+    def make_directory(self, path: Deque[str]):
+        if path:
+            first_directory = self.get_folder(path.popleft())
+        else:
+            first_directory = self
+
+        print(first_directory)
+
+    def print(self):
+        pass
 
 
 class SeventhDayTask(AdventOfCodeTask):
     def run(self):
         data = self.parameters.input[:-1]
 
+        file_system = FileSystem()
+
         last_command = ""
         output = []
 
-        current_directory = ""
+        current_directory = deque([])
 
         for line in data.split("\n"):
             if line.startswith('$'):
@@ -27,18 +65,18 @@ class SeventhDayTask(AdventOfCodeTask):
 
                 if command == 'cd':
                     if args[0] == '/':
-                        current_directory = '/'
+                        current_directory = deque([])
                     elif args[0] == '..':
-                        # current_directory = current_directory[1:-1]
-                        # current_directory = '/'.join(current_directory.split("/")[:-1])
-                        # current_directory = current_directory + '/'
-
-                        current_directory = '/'.join(current_directory.split("/")[:-2]) + '/'
+                        current_directory.pop()
                     else:
-                        current_directory += args[0] + '/'
+                        current_directory.append(args[0])
 
-                    print(f"Current directory: {current_directory}")
+                    # print(f"Current directory: {current_directory}")
+
+                    file_system.make_directory(copy.deepcopy(current_directory))
 
                 last_command = command
             else:
                 output.append(line)
+
+        print(file_system.print())
