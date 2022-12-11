f4c3ea98918bc05026a01e38a49a471c2b31f363
Libor Šafář <liborsaf@wetian.eu>
2022-12-11 03:12:09 +0100

    I give up for today
    Took 55 minutes

--- src/tasks/year_2022/ninth_day.py
+++ src/tasks/year_2022/ninth_day.py
@@ -1,149 +1,185 @@
-from enum import Enum
-from typing import List, Tuple
+from __future__ import annotations
+
+from typing import List, Tuple, Optional
 
 from tkinter import *
 # noinspection PyPackageRequirements
 from aoc import AdventOfCodeTask
 
 
-class Direction(Enum):
-    Up = 0,
-    Down = 1,
-    Left = 2,
-    Right = 3
+class Vector:
+    def __init__(self, x: int = 0, y: Optional[int] = None):
+        if not y:
+            y = x
+
+        self.x = x
+        self.y = y
+
+    def copy(self) -> Vector:
+        return Vector(self.x, self.y)
+
+    def __add__(self, other: Vector) -> Vector:
+        self.x += other.x
+        self.y += other.y
+
+        return self
+
+    # def __sub__(self, other: Vector) -> Vector:
+    #    self.x -= other.x
+    #    self.y -= other.y
+    #
+    #    return self
+
+    # def __abs__(self) -> Vector:
+    #    self.x = abs(self.x)
+    #     self.y = abs(self.y)
+    #
+    #    return self
+
+    def __mul__(self, other: int | Vector) -> Vector:
+        if isinstance(other, int):
+            self.x *= other
+            self.y *= other
+        elif isinstance(other, Vector):
+            self.x *= other.x
+            self.y *= other.y
+
+        return self
+
+    def __repr__(self):
+        return f"V({self.x}, {self.y})"
 
 
-class Node:
+class Rope:
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.head_coords = Vector()
+        self.tail_coords = self.head_coords.copy()
+
+        self.history = []
+
+    def move(self, vector: Vector, count: int):
+        for i in range(count):
+            self.head_coords += vector
+            self.history.append(('head', self.head_coords.copy()))
+
+        while abs(delta_x := self.head_coords.x - self.tail_coords.x) >= 2:
+            self.tail_coords.x += delta_x
+            self.history.append(('tail', self.tail_coords.copy()))
+
+        while abs(delta_y := self.head_coords.y - self.tail_coords.y) >= 2:
+            self.tail_coords.y += delta_y
+            self.history.append(('tail', self.tail_coords.copy()))
 
 
 class NinthDayTask(AdventOfCodeTask):
-    direction_mapping = {
-        'U': Direction.Up,
-        'D': Direction.Down,
-        'L': Direction.Left,
-        'R': Direction.Right
+    move_vector = {
+        'U': Vector(0, 1),
+        'D': Vector(0, -1),
+        'L': Vector(-1, 0),
+        'R': Vector(1, 0)
     }
 
-    pixel_size = 10
-
     def __init__(self):
-        self.head = Node()
-        self.tail = Node()
-
-        self.head_index = 0
-        self.tail_index = 0
-
-        self.step_history = []
-        self.tail_history = []
-
-        self.index = 0
-
-        self.root = None
-        self.canvas = None
-
-        self.head_coords = []
-        self.tail_coords = []
+        self.rope = Rope()
+        self.window = None
 
     def run(self):
-        data = self.parameters.input[:-1]
+        steps = self.parse_steps(self.parameters.input[:-1])
 
-        steps = self.parse_steps(data)
+        # print(steps)
 
-        for (direction, count) in steps:
-            if direction == Direction.Up:
-                self.head.y += count
-            elif direction == Direction.Down:
-                self.head.y -= count
-            elif direction == Direction.Left:
-                self.head.x += count
-            elif direction == Direction.Right:
-                self.head.x -= count
+        for step in steps:
+            self.process_step(step)
 
-            self.step_history.append(('head', (self.head.x, self.head.y)))
+        width = 0
+        height = 0
 
-            self.process_tail()
+        for step in self.rope.history:
+            (_, coords) = step
 
-        self.width = max([i[1][0] * self.pixel_size for i in self.step_history])
-        self.height = max([i[1][1] * self.pixel_size for i in self.step_history])
+            width = max(width, coords.x)
+            height = max(height, coords.y)
 
-        print(f"{self.width}x{self.height}")
+        self.window = Window(Vector(width * 3, height * 3))
+        self.window.steps_history = self.rope.history
 
-        self.root = Tk()
-        self.root.geometry(f"{self.width}x{self.height}")
-        self.root.config(bg="#000")
+        self.window.run()
 
-        self.canvas = Canvas(self.root, width=self.width, height=self.height, bg="#fff")
-        self.canvas.pack()
-
-        self.root.after(500, self.step)
-        self.root.mainloop()
-
-    def step(self):
-        (what, coords) = self.step_history[self.index]
-        (x, y) = coords
-
-        x += self.width // 2
-        y += self.height // 2
-
-        x *= self.pixel_size
-        y *= self.pixel_size
-
-        if what == 'tail':
-            self.tail_coords = (x, y)
-        elif what == 'head':
-            self.head_coords = (x, y)
-
-        print(f"Step (what={what}, coords={coords})")
-
-        self.canvas.delete("all")
-
-        if self.head_coords:
-            self.canvas.create_rectangle(self.head_coords[0], self.head_coords[1],
-                                         self.head_coords[0] + self.pixel_size, self.head_coords[1] + self.pixel_size,
-                                         fill='green')
-
-        if self.tail_coords:
-            self.canvas.create_rectangle(self.tail_coords[0], self.tail_coords[1],
-                                         self.tail_coords[0] + self.pixel_size, self.tail_coords[1] + self.pixel_size,
-                                         fill='red')
-
-        self.index += 1
-
-        self.root.after(500, self.step)
-
-    def process_tail(self):
-        while abs(difference_x := self.head.x - self.tail.x) >= 2:
-            self.tail.x += difference_x / abs(difference_x)
-            self.tail_history.append((self.tail.x, self.tail.y))
-            self.step_history.append(('tail', (self.tail.x, self.tail.y)))
-
-        while abs(difference_y := self.head.y - self.tail.y) >= 2:
-            self.tail.y += difference_y / abs(difference_y)
-            self.tail_history.append((self.tail.x, self.tail.y))
-            self.step_history.append(('tail', (self.tail.x, self.tail.y)))
-
-        while self.tail.x != self.head.x and self.tail.y != self.head.y:
-            is_negative_x = (self.head.x - self.tail.x) < 0
-            is_negative_y = (self.head.x - self.tail.y) < 0
-
-            self.tail.x += -1 if is_negative_x else 1
-            self.tail.y += -1 if is_negative_y else 1
-
-            self.tail_history.append((self.tail.x, self.tail.y))
-            self.step_history.append(('tail', (self.tail.x, self.tail.y)))
-
-    def parse_steps(self, data: str) -> List[Tuple[Direction, int]]:
+    def parse_steps(self, data: str) -> List[Tuple[int, Vector]]:
         steps = []
 
         for line in data.split("\n"):
             line = line.split(" ")
 
-            direction = self.direction_mapping[line[0]]
-            count = int(line[1])
-
-            steps.append((direction, count))
+            step = (int(line[1]), self.move_vector[line[0]])
+            steps.append(step)
 
         return steps
+
+    def process_step(self, step: Tuple[int, Vector]):
+        (count, vector) = step
+
+        self.rope.move(vector, count)
+
+
+class Window:
+    pixel_size = 10
+    playback_speed = 250
+
+    def __init__(self, size: Vector):
+        self.size = size
+        self.root = None
+        self.canvas = None
+
+        self.index = 0
+        self.steps_history = []
+        self.last_head_coords = None
+        self.last_tail_coords = None
+
+        self.setup_root()
+        self.setup_canvas()
+
+    def setup_root(self):
+        self.root = Tk()
+        self.root.geometry(f"{self.size.x}x{self.size.y}")
+        self.root.resizable(False, False)
+        self.root.config(bg="black")
+
+    def setup_canvas(self):
+        self.canvas = Canvas(self.root, width=self.size.x, height=self.size.y, bg="white")
+        self.canvas.pack()
+
+    def run(self):
+        self.root.after(self.playback_speed, self.loop)
+        self.root.mainloop()
+
+    def loop(self):
+        self.canvas.delete("all")
+
+        (node, coords) = self.steps_history[self.index]
+
+        if node == 'head':
+            self.last_head_coords = coords
+        elif node == 'tail':
+            self.last_tail_coords = coords
+
+        if self.last_head_coords:
+            self.draw(self.last_head_coords, "green")
+
+        if self.last_tail_coords:
+            self.draw(self.last_tail_coords, "red")
+
+        self.index += 1
+
+        self.root.after(self.playback_speed, self.loop)
+
+    def draw(self, coords: Vector, color: str, size: Vector = Vector(pixel_size)):
+
+        coords *= self.pixel_size
+
+        coords.x += self.size.x // 2
+        coords.y += self.size.y // 2
+
+        # print(coords)
+
+        self.canvas.create_rectangle(coords.x, coords.y, coords.x + size.x, coords.y + size.y, fill=color)
